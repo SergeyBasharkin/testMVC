53057246701c24123d89af9fbe7aab8bd30cc182
Stanislav <nuryelse@gmail.com>
2017-06-08 16:31:53 +0300

    Add files via upload

---
+++ src/main/java/com/flerry/newmvc/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.flerry.newmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello(ModelMap model) {
+        model.addAttribute("test", "Hello!");
+        System.out.println(model.toString());
+        return "index";
+    }
+}
